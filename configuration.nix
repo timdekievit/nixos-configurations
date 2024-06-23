3e5bf346394ff57ba0c90256e5f3441f9d013e87
tim <timdekievit@hotmail.com>
2024-06-23 19:35:58 +0200

    added a sound module

--- configuration.nix
+++ configuration.nix
@@ -128,15 +128,6 @@ in
   # rtkit is optional but recommended
   security.rtkit.enable = true;
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-  };
-
   # neccesary for nemo network to work
   services.gvfs.enable = true;
 
