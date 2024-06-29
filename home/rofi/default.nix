01c6d8b16f7ca1fbff8b0436d55eda737d2276c5
System administrator <timdekievit@hotmail.com>
2024-06-29 15:53:56 +0200

    rofi theming attempt

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -4,41 +4,41 @@
   ...
 }: {
   
-  # home.file.".local/sharerofi" = {
-  #   source = ./configs;
-  #   # copy the scripts directory recursively
-  #   recursive = true;
-  # };
+  home.file.".config/rofi" = {
+    source = ./configs;
+    # copy the scripts directory recursively
+    recursive = true;
+  };
 
   programs.rofi.enable = true;
-  programs.rofi.theme = 
-    let
-  # Use `mkLiteral` for string-like values that should show without
-  # quotes, e.g.:
-  # {
-  #   foo = "abc"; =&gt; foo: "abc";
-  #   bar = mkLiteral "abc"; =&gt; bar: abc;
-  # };
-  inherit (config.lib.formats.rasi) mkLiteral;
-in {
-  "*" = {
-    background-color = mkLiteral "#000000";
-    foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
-    border-color = mkLiteral "#FFFFFF";
-    width = 512;
-  };
+#   programs.rofi.theme = 
+#     let
+#   # Use `mkLiteral` for string-like values that should show without
+#   # quotes, e.g.:
+#   # {
+#   #   foo = "abc"; =&gt; foo: "abc";
+#   #   bar = mkLiteral "abc"; =&gt; bar: abc;
+#   # };
+#   inherit (config.lib.formats.rasi) mkLiteral;
+# in {
+#   "*" = {
+#     background-color = mkLiteral "#000000";
+#     foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
+#     border-color = mkLiteral "#FFFFFF";
+#     width = 512;
+#   };
 
-  "#inputbar" = {
-    children = map mkLiteral [ "prompt" "entry" ];
-  };
+#   "#inputbar" = {
+#     children = map mkLiteral [ "prompt" "entry" ];
+#   };
 
-  "#textbox-prompt-colon" = {
-    expand = false;
-    str = ":";
-    margin = mkLiteral "0px 0.3em 0em 0em";
-    text-color = mkLiteral "@foreground-color";
-  };
+#   "#textbox-prompt-colon" = {
+#     expand = false;
+#     str = ":";
+#     margin = mkLiteral "0px 0.3em 0em 0em";
+#     text-color = mkLiteral "@foreground-color";
+#   };
 
-
+    programs.rofi.theme = ".config/rofi/powermenu.rasi"
   };
 }
