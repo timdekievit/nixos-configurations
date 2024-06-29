9fefd5b079ab92ea75cd22e3656aa109d7aedbcc
System administrator <timdekievit@hotmail.com>
2024-06-29 12:58:28 +0200

    changed wallpaper config in rc.lua

--- home/awesome-wm/rc.lua
+++ home/awesome-wm/rc.lua
@@ -154,12 +154,12 @@ local tasklist_buttons = gears.table.join(
 local function set_wallpaper(s)
     -- Wallpaper
     if beautiful.wallpaper then
-        local wallpaper = './wallpaper.jpg'
+        local wallpaper = beautiful.wallpaper
         -- If wallpaper is a function, call it with the screen
         if type(wallpaper) == "function" then
             wallpaper = wallpaper(s)
         end
-        gears.wallpaper.maximized(wallpaper, s, true)
+        gears.wallpaper.maximized('./wallpaper.jpg', s, true)
     end
 end
 
