f2e7e9dcaf29de7936cd7fe9b40ae5d85d74c76a
tim <timdekievit@hotmail.com>
2024-06-29 22:55:54 +0200

    removed comments

--- hosts/Asus-laptop/default.nix
+++ hosts/Asus-laptop/default.nix
@@ -1,17 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, lib, pkgs, ... }:
 
-# let
-#   unstable = import (fetchTarball {
-#     url = "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz";
-#   }) { config = { allowUnfree = true; }; };
-# in
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
       ../../modules/nvidia.nix
       ../../modules/awesome-wm.nix
