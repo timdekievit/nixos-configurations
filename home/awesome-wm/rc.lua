3f9cc41229e226ace064f10c78b03f536c7456b4
System administrator <timdekievit@hotmail.com>
2024-06-29 12:50:44 +0200

    added wallpaper attempt

--- home/awesome-wm/rc.lua
+++ home/awesome-wm/rc.lua
@@ -154,7 +154,7 @@ local tasklist_buttons = gears.table.join(
 local function set_wallpaper(s)
     -- Wallpaper
     if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
+        local wallpaper = './wallpaper.jpg'
         -- If wallpaper is a function, call it with the screen
         if type(wallpaper) == "function" then
             wallpaper = wallpaper(s)
