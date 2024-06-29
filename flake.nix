90f298564b3fdd91e0350efbe0d050b17ce33854
tim <timdekievit@hotmail.com>
2024-06-29 22:46:59 +0200

    added stylix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    stylix.url = "github:danth/stylix";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -13,6 +14,7 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/Asus-laptop/default.nix
+          stylix.nixosModules.stylix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
---
+++ modules/stylix.nix
@@ -0,0 +1,6 @@
+  { config, lib, ...}:
+
+{
+ stylix.enable = true;
+ stylix.image = ../home/awesome-wm/wallpaper.jpg;
+}
