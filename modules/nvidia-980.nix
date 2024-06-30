934e056efba28f1169011402996f1e2aeae0fc5d
tim <timdekievit@hotmail.com>
2024-06-30 18:41:59 +0200

    nvidia 980 setup with lg ultrawide xrandr

--- modules/nvidia-980.nix
+++ modules/nvidia-980.nix
@@ -8,7 +8,10 @@
   hardware.nvidia.forceFullCompositionPipeline = true;
 
   services.xserver.videoDrivers = ["nvidia"];
-  services.xserver.displayManager.setupCommands = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 2560x1080+0+0 --output HDMI-0 --off '';
+    services.xserver.displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 2560x1080 --rate 144 --fbmm 600x340 --dpi 96 --output HDMI-0 --off
+
+  '';
 
   # Enable OpenGL
   hardware.graphics = {
