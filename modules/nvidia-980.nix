f414ba6c7ea6334ef08bc06e7623c55aeba2a72d
tim <timdekievit@hotmail.com>
2024-06-30 17:34:01 +0200

    xrandr config attempt

--- modules/nvidia-980.nix
+++ modules/nvidia-980.nix
@@ -8,6 +8,7 @@
   hardware.nvidia.forceFullCompositionPipeline = true;
 
   services.xserver.videoDrivers = ["nvidia"];
+  services.displayManager.setupCommands = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 2560x1080 --pos 0x0 --rotate normal --output HDMI-0 --off '';
 
   # Enable OpenGL
   hardware.graphics = {
