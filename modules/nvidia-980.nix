1dccc4ed18ed53f76d9499b17a722f728f3eedf2
tim <timdekievit@hotmail.com>
2024-06-30 17:40:18 +0200

    ateempt 3

--- modules/nvidia-980.nix
+++ modules/nvidia-980.nix
@@ -8,7 +8,7 @@
   hardware.nvidia.forceFullCompositionPipeline = true;
 
   services.xserver.videoDrivers = ["nvidia"];
-  services.xserver.displayManager.setupCommands = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 2560x1080 --pos 0x0 --rotate normal2 --output HDMI-0 --off '';
+  services.xserver.displayManager.setupCommands = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 2560x1080 --pos 0x0 --rotate normal --output HDMI-0 --off '';
 
   # Enable OpenGL
   hardware.graphics = {
