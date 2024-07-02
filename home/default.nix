3e1a6771c9b89634f2cbb7e5f6e4046dd8ebb50d
tim <timdekievit@hotmail.com>
2024-07-02 21:36:01 +0200

    icon theming attempt 4

--- home/default.nix
+++ home/default.nix
@@ -29,8 +29,8 @@
 
   gtk.enable = true;
 
-  gtk.theme.package = pkgs.adw-gtk3;
-  gtk.theme.name = "adw-gtk3";
+  # gtk.theme.package = pkgs.adw-gtk3;
+  # gtk.theme.name = "adw-gtk3";
 
   gtk.iconTheme = {
       name = "Papirus-Dark";
