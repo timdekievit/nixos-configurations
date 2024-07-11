a6906b29048402a8c3fd014f3f6450053b02a582
tim <timdekievit@hotmail.com>
2024-07-11 13:11:32 +0200

    adding flameshot a screenshot tool

--- home/default.nix
+++ home/default.nix
@@ -70,6 +70,7 @@
       picard
       yt-dlp
       mpv
+      flameshot
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
