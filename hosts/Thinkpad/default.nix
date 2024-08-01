037589cc8a1b12c8bfdad264258987400c76b681
Tim de Kievit <timdekievit@hotmail.com>
2024-08-01 16:42:37 +0200

    adding appimages support

--- hosts/Thinkpad/default.nix
+++ hosts/Thinkpad/default.nix
@@ -122,6 +122,16 @@
 
   services.mullvad-vpn.enable = true;
 
+  # allows for running appimages
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+};
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
