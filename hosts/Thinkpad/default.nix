4df2a0a99319e945d4f5489ee61f6f620ce3dbb9
Tim de Kievit <timdekievit@hotmail.com>
2024-08-11 18:48:24 +0200

    enabled experimental features

--- hosts/Thinkpad/default.nix
+++ hosts/Thinkpad/default.nix
@@ -10,6 +10,8 @@
       ./hardware-configuration.nix
     ];
 
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
