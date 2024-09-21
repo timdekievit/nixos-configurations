325ca0060248f7cb489745298306ea8dc6eec447
Tim de Kievit <timdekievit@hotmail.com>
2024-09-21 17:10:13 +0200

    adding gaming apps

--- hosts/Thinkpad/default.nix
+++ hosts/Thinkpad/default.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../modules/gaming.nix
     ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
