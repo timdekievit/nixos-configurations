5d9f85af7c0766cb1f4788b1f1db5acd86e210ec
tim <timdekievit@hotmail.com>
2024-11-26 22:32:08 +0100

    add appimage support

--- hosts/Desktop-pc/default.nix
+++ hosts/Desktop-pc/default.nix
@@ -92,6 +92,16 @@
     dotnet-sdk_8
   ];
 
+  # allows for running appimages
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+};
+
   # rtkit is optional but recommended
   security.rtkit.enable = true;
 
